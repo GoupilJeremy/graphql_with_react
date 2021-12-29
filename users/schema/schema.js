211d11f20d0d17f29d08b45efa08018c8651285a
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-29 11:08:51 +0100

    The GraphQL tool #6

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,8 +8,8 @@ const {
 } = graphql;
 
 const users = [
-    { id: '23', firstname: 'Bill', age: 20},
-    { id: '47', firstname: 'Samantha', age: 21}
+    { id: '23', firstName: 'Bill', age: 20},
+    { id: '47', firstName: 'Samantha', age: 21}
 ];
 const UserType = new GraphQLObjectType({
     name: 'User',
