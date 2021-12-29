c48edecf7945b1b8dfc0a6d11367b63b19ab1f3f
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-29 10:43:32 +0100

    Root queries #4 (#15)

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -13,3 +13,16 @@ const UserType = new GraphQLObjectType({
         age: { type: GraphQLInt}
     }
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                
+            }
+        }
+    }
+});
