e3ffca02ca42364e760bef28be1f780b5f8d5066
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-29 10:53:21 +0100

    Resolving with Data #5 (#16)

--- users/server.js
+++ users/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true
 }));
 
